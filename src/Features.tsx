900854a2c288a22ccf20b454b21701efc2522c38
Teo Theodoridis <teo.godgift4@gmail.com>
2021-09-05 22:50:05 +0300

    fetching json data

--- src/Features.tsx
+++ src/Features.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export default function Features() {
+  async function fetchFeatures() {
+    const response = await fetch('https://www.cretalive.gr/api/news-feed?_format=json&page=0');
+    const features_list = await response.json();
+    return features_list;
+  }
+  fetchFeatures().then(features_list=>{
+      console.log(features_list)
+  })
     return (
         <div>
-            features component
+            <p>features component</p>
         </div>
     )
 }
--- src/Latest.tsx
+++ src/Latest.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 export default function Latest() {
+    async function fetchLatest() {
+        const response = await fetch('https://www.cretalive.gr/api/todays-popular?_format=json');
+        const latest_list = await response.json();
+        return latest_list;
+      }
+      fetchLatest().then(latest_list=>{
+          console.log(latest_list)
+      })
     return (
         <div>
             latest component
